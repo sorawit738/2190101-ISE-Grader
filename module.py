308dfe2b36a799dc02a0f1fda2897f372de759e7
ICE <ice.sorawit23@gmail.com>
2023-08-27 17:23:18 +0700

    it just works

--- module.py
+++ module.py
@@ -7,3 +7,63 @@ def insertionSort(x):
             j -= 1
         x[j + 1] = temp
     return x
+
+def selectionSort(x):
+    for i in range(len(x)):
+        position = i
+        for j in range((i + 1), len(x)):
+            if x[j] < x[position]:
+                position = j
+        x[i], x[position] = x[position], x[i]
+    return x
+
+def merge(arr, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    left = [0] * (n1)
+    right = [0] * (n2)
+    for i in range(n1):
+        left[i] = arr[p + i]
+    for j in range(n2):
+        right[j] = arr[q + 1 + j]
+
+    i, j, k = 0, 0, p
+    while i < n1 and j < n2:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+    while i < n1:
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < n2:
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+def mergeSort(arr, p, r):
+    if p < r:
+        q = int(p + (r - p) / 2)
+        mergeSort(arr, p, q)
+        mergeSort(arr, q + 1, r)
+        merge(arr, p, q, r)
+
+def partition(x, p, r):
+    temp = x[r]
+    i = p - 1
+    for j in range(p, r):
+        if x[j] <= temp:
+            i += 1
+            x[i], x[j] = x[j], x[i]
+    x[r], x[i + 1] = x[i + 1], x[r]
+    return i + 1
+
+def quickSort(x, p, r):
+    if p < r:
+        q = partition(x, p, r)
+        quickSort(x, p, q - 1)
+        quickSort(x, q + 1, r)
---
+++ random_1.py
@@ -0,0 +1,28 @@
+import module
+import time
+import random
+
+# Generate an array of 200 unordered integers
+
+array_size = 40000000
+min_value = 0
+max_value = 10000
+unordered_array = [random.randint(min_value, max_value) for _ in range(array_size)]
+
+print(unordered_array)
+print("\n")
+
+arr = [0, -1, -2, 2, 1]
+
+start = time.time()
+
+module.quickSort(unordered_array, 0, len(arr) - 1)
+print(unordered_array)
+#print(module.insertionSort(unordered_array))
+#print(module.selectionSort(unordered_array))
+#module.mergeSort(unordered_array, 0, len(unordered_array) - 1)
+#print(unordered_array)
+
+end = time.time()
+print("\n")
+print("--- %s miliseconds ---" % ((end - start) * 1000))
